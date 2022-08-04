972ba16d1eef5ab1ddd37a7c147b536e8a14de41
Allison Ause <allison.ause@gmail.com>
2022-08-03 17:56:10 -0700

    logging off mid id test

--- __tests__/yarn.test.js
+++ __tests__/yarn.test.js
@@ -8,9 +8,20 @@ describe('yarn routes', () => {
     return setup(pool);
   });
   it('#GET /yarn displays list of yarn', async () => {
-    const res = await request(app).get('/yarn');
+    const res = await request(app).get('/');
     expect(res.status).toBe(200);
     expect(res.body.length === 4);
+  });
+
+  it('#GET /yarn/:id displays single yarn', async () => {
+    const res = request(app).get('/:id');
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id: '1',
+      brand: 'Woolstock', 
+      fiber: 'Wool', 
+      weight: 'DK', 3)    
+    });
   });
 });
 afterAll(() => {
